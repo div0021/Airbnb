730d6bbb31b5b00c08370697ab6d171ab3544586
div0021 <div2wk@gmail.com>
2023-07-08 02:49:00 +0530

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import LoginModal from './components/modals/LoginModal'
 import getCurrentUser from './actions/getCurrentUser'
 import RentModal from './components/modals/RentModal'
 import SearchModal from './components/modals/SearchModal'
+import { Suspense } from 'react'
 
 export const metadata = {
   title: 'Airbnb',
@@ -25,6 +26,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Suspense fallback={<p>Loading...</p>}>
         <Toaster />
         <SearchModal />
         <RentModal />
@@ -35,7 +37,7 @@ export default async function RootLayout({
         <div className='pb-20 pt-28'>
               {children}
         </div>
-
+        </Suspense>
         </body>
     </html>
   )
